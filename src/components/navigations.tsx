34200f1b446fbaf09c4e414f5e13d7da79c900fe
chulakasam <sampathchulaka33872@gmail.com>
2025-01-09 13:31:20 +0530

    add tailwind

--- src/components/navigations.tsx
+++ src/components/navigations.tsx
@@ -1,12 +1,13 @@
 import {Link} from "react-router";
-
+import './navigation.css'
 
 export function Navigation(){
     return (
         <>
-            <header>
-                <nav>
-                    <ul>
+            <header className='bg-teal-900'>
+                <nav className='px-4 py-5'>
+                    <ul className='flex text-white space-x-4'>
+
                         <Link to="/">DashBoard</Link>
                         <Link to="/staff">Staff</Link>
                         <Link to="/field">Field</Link>
