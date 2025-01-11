74652754a0d86ee4f1eab58203bda62a21e9aefb
chulakasam <sampathchulaka33872@gmail.com>
2025-01-11 09:29:52 +0530

    add equipment form

--- src/pages/addEquipment.tsx
+++ src/pages/addEquipment.tsx
@@ -5,8 +5,12 @@ export function AddEquipment() {
             <div>
                 <h2>Add New Equipment</h2>
                 <form>
+                    <label>Equipment ID: <input type="text" name="equipment_id"/></label><br/>
                     <label>Equipment Name: <input type="text" name="name"/></label><br/>
                     <label>Type: <input type="text" name="type"/></label><br/>
+                    <label>Status: <input type="text" name="status"/></label><br/>
+                    <label>Staff ID: <input type="text" name="staff_id"/></label><br/>
+                    <label>Field Code: <input type="text" name="field_code"/></label><br/>
                     <button type="submit">Add Equipment</button>
                 </form>
             </div>
