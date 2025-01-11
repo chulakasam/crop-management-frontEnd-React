ac68706267617a1d284dbc85fce4d633763c57b6
chulakasam <sampathchulaka33872@gmail.com>
2025-01-11 11:10:36 +0530

    add vehicle form

--- src/pages/addVehicle.tsx
+++ src/pages/addVehicle.tsx
@@ -5,8 +5,13 @@ export function AddVehicle(){
             <div>
                 <h2>Add New Vehicle</h2>
                 <form>
-                    <label>Vehicle Name: <input type="text" name="name"/></label><br/>
                     <label>License No: <input type="text" name="license_no"/></label><br/>
+                    <label>Vehicle code: <input type="text" name="vehicle_code"/></label><br/>
+                    <label>Category: <input type="text" name="category"/></label><br/>
+                    <label>Status: <input type="text" name="status"/></label><br/>
+                    <label>Fuel type: <input type="text" name="fuel_type"/></label><br/>
+                    <label>Remark: <input type="text" name="remark"/></label><br/>
+
                     <button type="submit">Add Vehicle</button>
                 </form>
             </div>
