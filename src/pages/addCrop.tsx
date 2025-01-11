8b8e1756180571ea2dab553ca4686965f31fcead
chulakasam <sampathchulaka33872@gmail.com>
2025-01-11 09:35:13 +0530

    add crop form

--- src/pages/addCrop.tsx
+++ src/pages/addCrop.tsx
@@ -5,8 +5,13 @@ export function AddCrop() {
             <div>
                 <h2>Add New Crop</h2>
                 <form>
+                    <label>Crop ID: <input type="text" name="crop_id"/></label><br/>
                     <label>Crop Name: <input type="text" name="crop_name"/></label><br/>
-                    <label>Type: <input type="text" name="type"/></label><br/>
+                    <label>Scientific Name: <input type="text" name="scientific_name"/></label><br/>
+                    <label>Image: <input type="text" name="image"/></label><br/>
+                    <label>Category: <input type="text" name="category"/></label><br/>
+                    <label>Season: <input type="text" name="season"/></label><br/>
+                    <label>Field Code: <input type="text" name="field_code"/></label><br/>
                     <button type="submit">Add Crop</button>
                 </form>
             </div>
