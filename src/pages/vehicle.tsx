8a0fffc7b3bdeef1f73b23183fa13a24cd0d548b
chulakasam <sampathchulaka33872@gmail.com>
2025-01-14 12:26:43 +0530

    vehicle update,search method done

--- src/pages/vehicle.tsx
+++ src/pages/vehicle.tsx
@@ -2,12 +2,23 @@ import {Link} from "react-router";
 import {useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import * as React from "react";
-import {deleteVehicle} from "../slice/VehicleSlice.ts";
+import {deleteVehicle, updateVehicle} from "../slice/VehicleSlice.ts";
 
 export function Vehicle() {
     const vehicle = useSelector((state:any) => state.vehicle);
     const dispatch = useDispatch();
     const [deleteLicenseNo,setDeleteLicenseNo] = useState('');
+
+
+    const [searchLicenseNo, setSearchLicenseNo] = useState('');
+    const [foundVehicle, setFoundVehicle] = useState<any | null>(null);
+
+    const [newVehicleCode, setNewVehicleCode] = useState('');
+    const [newCategory, setNewCategory] = useState('');
+    const [newStatus, setNewStatus] = useState('');
+    const [newFuelType, setNewFuelType] = useState('');
+    const [newRemark, setNewRemark] = useState('');
+
 
 
 
@@ -22,6 +33,41 @@ export function Vehicle() {
 
     }
 
+    function handleSearchVehicle(event:React.FormEvent){
+        event.preventDefault();
+        const found = vehicle.find((v: any) => v.licenseNo === searchLicenseNo);
+        if (found) {
+            setFoundVehicle(found);
+            setNewVehicleCode(found.vehicleCode);
+            setNewCategory(found.category);
+            setNewStatus(found.status);
+            setNewFuelType(found.fuelType);
+            setNewRemark(found.remark);
+
+        } else {
+            alert('vehicle not found.');
+            setFoundVehicle(null);
+        }
+    }
+
+    function handleUpdateVehicle(event:React.FormEvent){
+        event.preventDefault();
+        if(foundVehicle){
+            dispatch(updateVehicle({licenseNo:foundVehicle.licenseNo,newVehicleCode,newCategory,newStatus,newFuelType,newRemark}));
+
+            alert("vehicle updated successfully.");
+            setNewVehicleCode('');
+            setNewCategory('');
+            setNewStatus('');
+            setNewFuelType('');
+            setNewRemark('');
+        }else{
+            alert("vehicle not found.");
+            setFoundVehicle(null);
+        }
+    }
+
+
     return (
         <>
             <br/>
@@ -32,19 +78,43 @@ export function Vehicle() {
                 <button>Add New Vehicle</button>
             </Link>
             {/*TODO ----------------function update*/}
-            <button>Update Vehicle</button>
+            <input type="text" placeholder="License No to search" value={searchLicenseNo}
+                   onChange={(e) => setSearchLicenseNo(e.target.value)}/>
 
+            <button onClick={handleSearchVehicle}>Search Vehicle</button>
 
-
+            {foundVehicle && (
+                <div>
+                    <h3>Update Vehicle:</h3>
+                    <p>
+                        <strong>Current Vehicle No:</strong> {foundVehicle.vehicleCode}
+                        <br/>
+                        <strong>Current Category:</strong> {foundVehicle.category}
+                        <br/>
+                        <strong>Current Status:</strong> {foundVehicle.status}
+                        <br/>
+                        <strong>Current Fuel type:</strong> {foundVehicle.fuelType}
+                        <br/>
+                        <strong>Current Remark:</strong> {foundVehicle.remark}
+                        <br/>
+                    </p>
+                    <input type="text" placeholder="New vehicle code" value={newVehicleCode} onChange={(e) => setNewVehicleCode(e.target.value)}/>
+                    <input type="text" placeholder="New category" value={newCategory} onChange={(e) => setNewCategory(e.target.value)}/>
+                    <input type="text" placeholder="New status" value={newStatus} onChange={(e) => setNewStatus(e.target.value)}/>
+                    <input type="text" placeholder="New fuel type" value={newFuelType} onChange={(e) => setNewFuelType(e.target.value)}/>
+                    <input type="text" placeholder="New remark" value={newRemark} onChange={(e) => setNewRemark(e.target.value)}/>
+                    <button onClick={handleUpdateVehicle}>Update vehicle</button>
+                </div>
+            )}
 
 
 
             {/*TODO ----------------function delete*/}
             <br/>
 
-            <input type="text" placeholder="Enter Vehicle License No" value={deleteLicenseNo} onChange={(e) => setDeleteLicenseNo(e.target.value)}/>
+            <input type="text" placeholder="Enter Vehicle License No" value={deleteLicenseNo}
+                   onChange={(e) => setDeleteLicenseNo(e.target.value)}/>
             <button onClick={handleDeleteVehicle}>Delete Vehicle</button>
-
 
 
             <br/>
--- src/slice/VehicleSlice.ts
+++ src/slice/VehicleSlice.ts
@@ -10,8 +10,20 @@ const VehicleSlice = createSlice({
         },
         deleteVehicle:(state,action)=>{
             return state.filter((vehicle) => vehicle.licenseNo !== action.payload);
+        },
+        updateVehicle:(state,action)=>{
+            const {licenseNo,newVehicleNo,newCategory,newStatus,newFuelType,newRemark} = action.payload;
+            const vehicle=state.find((vehicle) => vehicle.licenseNo === licenseNo);
+            if(vehicle){
+                vehicle.vehicleCode=newVehicleNo || vehicle.vehicleCode;
+                vehicle.category=newCategory || vehicle.categor;
+                vehicle.status=newStatus || vehicle.status;
+                vehicle.fuelType=newFuelType || vehicle.fuelType;
+                vehicle.remark=newRemark || vehicle.remark;
+
+            }
         }
     }
 })
 export  default VehicleSlice.reducer;
-export const {addNewVehicle,deleteVehicle} = VehicleSlice.actions;
+export const {addNewVehicle,deleteVehicle,updateVehicle} = VehicleSlice.actions;
